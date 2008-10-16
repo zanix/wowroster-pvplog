d1f8d08af42e9fd7af6a947ab6f3688c6fb5ef10
Zanix <Zanix@3dfb0d9d-1337-0e9c-4f91-0ea1b296e081>
2008-10-16 02:23:46 +0000

    GuildProfiler: - Tag 3.0.0

---
+++ tags/GuildProfiler/3.0.0/localization.lua
@@ -0,0 +1,11 @@
+RPGOGP_SAVE_TEXT		= "save";
+RPGOGP_SAVE_TOOLTIP		= "Click to export your Guild Profile";
+
+local myLocale=GetLocale();
+if(myLocale=="deDE") then
+	RPGOGP_SAVE_TEXT		= "save";
+elseif(myLocale=="frFR") then
+	RPGOGP_SAVE_TEXT		= "save";
+elseif(myLocale=="esES") then
+	RPGOGP_SAVE_TEXT		= "save";
+end
